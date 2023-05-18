21efae24a92b1d334cd103ec4be182aa79b04d26
Skeyne <andremrodrigues99@gmail.com>
2023-05-18 12:24:16 +0100

    ui polish

--- app.py
+++ app.py
@@ -133,9 +133,7 @@ def plot_altitude(df):
         b=0, #bottom margin
         t=30  #top margin
     ),
-    autosize=False,
-    width=520,
-    height=390,
+    autosize=True,
 )
 
     fig = go.Figure(
