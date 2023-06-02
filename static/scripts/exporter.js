1ca0c72de9517410167727141913e686501f32f2
Skeyne <andremrodrigues99@gmail.com>
2023-06-02 12:14:08 +0100

    gh pages static graphs

--- static/scripts/exporter.js
+++ static/scripts/exporter.js
@@ -1,33 +1,17 @@
-
 function getChartState (id) {
 const el = document.getElementById(id)
 return {
-    data: el.data, // current data
-    layout: el.layout // current layout
+    data: el.data,
+    layout: el.layout,
+    config: el._context
 }
 }
-
 async function getHtml(id) {
-    const state = getChartState(id)
 
-    return `
-        <head>
-            <meta charset="utf-8" />
-            <script src="https://cdn.plot.ly/plotly-2.20.0.min.js"></script>
-        </head>
-        <div id="${id}"></div>
-        
-        <script type="text/javascript">
-            Plotly.newPlot(
-            '${id}', 
-            ${JSON.stringify(state.data)},
-            ${JSON.stringify(state.layout)}
-            )
-        <\/script>
-`
+    return $(document.getElementById(id)).html()
 }
 
-async function exportToHtml (id) {
+async function downloadHtml (id) {
     // Create URL
     const blob = new Blob([await getHtml(id)], { type: 'text/html' })
     const url = URL.createObjectURL(blob)
@@ -35,7 +19,39 @@ async function exportToHtml (id) {
     // Create downloader
     const downloader = document.createElement('a')
     downloader.href = url
-    downloader.download = 'export.html'
+    downloader.download = `${id}.html`
+  
+    // Trigger click
+    downloader.click()
+  
+    // Clean up
+    URL.revokeObjectURL(url)
+  }
+
+async function getGraphHtml(id) {
+    const state = getChartState(id)
+
+    return `
+        <script type="text/javascript">
+            Plotly.newPlot(
+            '${id}', 
+            ${JSON.stringify(state.data)},
+            ${JSON.stringify(state.layout)},
+            ${JSON.stringify(state.config)}
+            )
+        <\/script>
+`
+}
+
+async function exportToHtml (id) {
+    // Create URL
+    const blob = new Blob([await getGraphHtml(id)], { type: 'text/html' })
+    const url = URL.createObjectURL(blob)
+  
+    // Create downloader
+    const downloader = document.createElement('a')
+    downloader.href = url
+    downloader.download = `${id}.html`
   
     // Trigger click
     downloader.click()
